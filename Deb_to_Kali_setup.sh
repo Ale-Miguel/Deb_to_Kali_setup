858735ad0c9824b2bb34609e7eff49949d9875dd
Ale Miguel <alemiguel@Ale-MacStudio.local>
2023-12-31 14:14:15 -0600

    First script installs all including GUI

---
+++ Deb_to_Kali_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' >>  /etc/apt/sources.list
+sudo wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-keyring.asc
+sudo apt update
+sudo apt upgrade -y && sudo apt full-upgrade -y && sudo atp autoremove -y && sudo apt autoclean -y
+sudo apt install kali-desktop-xfce -y
+sudo apt-get install xrdp kali-linux-headless kali-tools-web kali-desktop-xfce
+sudo systemctl enable xrdp
+reboot
