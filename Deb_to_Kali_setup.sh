ede09a0467b905eda0f4edeeb3dce72760bdf67e
Ale-Miguel <39611470+Ale-Miguel@users.noreply.github.com>
2024-01-03 23:13:18 -0600

    Removed duplicate installation of DE

--- Deb_to_Kali_setup.sh
+++ Deb_to_Kali_setup.sh
@@ -4,7 +4,6 @@ sudo echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' >> 
 sudo wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-keyring.asc
 sudo apt update
 sudo apt upgrade -y && sudo apt full-upgrade -y && sudo atp autoremove -y && sudo apt autoclean -y
-sudo apt install kali-desktop-xfce -y
-sudo apt-get install xrdp kali-linux-headless kali-tools-web kali-desktop-xfce
+sudo apt-get install xrdp kali-linux-headless kali-tools-web kali-desktop-xfce -y
 sudo systemctl enable xrdp
 reboot
